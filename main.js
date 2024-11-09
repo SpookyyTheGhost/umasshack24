d8fc1aec56a340c3ea57cb98656f15c8e30bdc0b
SpookyTheGhost <69209990+SpookyyTheGhost@users.noreply.github.com>
2024-11-09 13:34:17 -0500

    Added Text
    currently invisible

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 import './style.css'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 
 const scene = new THREE.Scene();
 
 scene.background = new THREE.Color(0x121212);
+
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -15,7 +18,7 @@ document.body.appendChild( renderer.domElement );
 
 
 const geometry = new THREE.BoxGeometry( 1,1,1 ); 
-const material = new THREE.PointsMaterial( { color: 0xffffff, wireframe:true});
+const material = new THREE.PointsMaterial( { color: 0xffffff});
 material.size = 0.04;
 const cube = new THREE.Points( geometry, material );
 
@@ -29,6 +32,32 @@ controls.minDistance = 2; // Minimum zoom distance
 controls.maxDistance = 7; // Maximum zoom distance
 
 camera.position.z = 5;
+
+// Load the font
+const loader = new FontLoader();
+loader.load('/fonts/droid_sans_regular.typeface.json', function (font) {
+        console.log('Font loaded successfully');
+        const textGeometry = new TextGeometry('VER.TECH', {
+            font: font,
+            size: 1,
+            height: 0.2,
+            curveSegments: 0.005,
+            bevelEnabled: false,
+            bevelThickness: 0.03,
+            bevelSize: 0.02,
+            bevelOffset: 0,
+            bevelSegments: 5
+        });
+        textGeometry.center();
+        const textMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe:true, transparent:true,opacity:0}); //currently invis        const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+        scene.add(textMesh);
+    },
+    undefined,
+    function (error) {
+        console.error('An error occurred loading the font', error);
+    }
+);
+
 
 window.addEventListener('resize', () => {
 	const width = window.innerWidth;
